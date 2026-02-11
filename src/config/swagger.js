832f55c164239828770dc78b66d8443bea3f46a9
Sujal Nage <sujal.nage@softude.com>
2026-02-11 19:28:17 +0530

    Fix Swagger Js Url

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:5000"
+        url: "https://online-bookstore-dwl5.onrender.com"
       }
     ],
     components: {
